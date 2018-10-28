254eaf78506d65f7e4dbffa67a6292dc2e23bf84
Alessandro Inouye de Albuquerque <morfeuw3@outlook.com>
2018-10-28 15:47:33 -0200

    Jogo da velha finalizado

---
+++ Jogo da Velha/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Jogo_da_Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b11 = new System.Windows.Forms.Button();
+            this.b12 = new System.Windows.Forms.Button();
+            this.b13 = new System.Windows.Forms.Button();
+            this.b21 = new System.Windows.Forms.Button();
+            this.b22 = new System.Windows.Forms.Button();
+            this.b23 = new System.Windows.Forms.Button();
+            this.b31 = new System.Windows.Forms.Button();
+            this.b32 = new System.Windows.Forms.Button();
+            this.b33 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b11
+            // 
+            this.b11.BackColor = System.Drawing.Color.White;
+            this.b11.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b11.Location = new System.Drawing.Point(12, 48);
+            this.b11.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b11.Name = "b11";
+            this.b11.Size = new System.Drawing.Size(150, 150);
+            this.b11.TabIndex = 0;
+            this.b11.UseVisualStyleBackColor = false;
+            // 
+            // b12
+            // 
+            this.b12.BackColor = System.Drawing.Color.White;
+            this.b12.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b12.Location = new System.Drawing.Point(190, 48);
+            this.b12.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b12.Name = "b12";
+            this.b12.Size = new System.Drawing.Size(150, 150);
+            this.b12.TabIndex = 1;
+            this.b12.UseVisualStyleBackColor = false;
+            // 
+            // b13
+            // 
+            this.b13.BackColor = System.Drawing.Color.White;
+            this.b13.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b13.Location = new System.Drawing.Point(370, 48);
+            this.b13.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b13.Name = "b13";
+            this.b13.Size = new System.Drawing.Size(150, 150);
+            this.b13.TabIndex = 2;
+            this.b13.UseVisualStyleBackColor = false;
+            // 
+            // b21
+            // 
+            this.b21.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b21.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b21.Location = new System.Drawing.Point(12, 223);
+            this.b21.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b21.Name = "b21";
+            this.b21.Size = new System.Drawing.Size(150, 150);
+            this.b21.TabIndex = 5;
+            this.b21.UseVisualStyleBackColor = false;
+            // 
+            // b22
+            // 
+            this.b22.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b22.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b22.Location = new System.Drawing.Point(190, 223);
+            this.b22.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b22.Name = "b22";
+            this.b22.Size = new System.Drawing.Size(150, 150);
+            this.b22.TabIndex = 4;
+            this.b22.UseVisualStyleBackColor = false;
+            // 
+            // b23
+            // 
+            this.b23.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b23.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b23.Location = new System.Drawing.Point(370, 223);
+            this.b23.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b23.Name = "b23";
+            this.b23.Size = new System.Drawing.Size(150, 150);
+            this.b23.TabIndex = 3;
+            this.b23.UseVisualStyleBackColor = false;
+            // 
+            // b31
+            // 
+            this.b31.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b31.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b31.Location = new System.Drawing.Point(12, 399);
+            this.b31.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b31.Name = "b31";
+            this.b31.Size = new System.Drawing.Size(150, 150);
+            this.b31.TabIndex = 8;
+            this.b31.UseVisualStyleBackColor = false;
+            // 
+            // b32
+            // 
+            this.b32.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b32.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b32.Location = new System.Drawing.Point(190, 399);
+            this.b32.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b32.Name = "b32";
+            this.b32.Size = new System.Drawing.Size(150, 150);
+            this.b32.TabIndex = 7;
+            this.b32.UseVisualStyleBackColor = false;
+            // 
+            // b33
+            // 
+            this.b33.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b33.Font = new System.Drawing.Font("Modern No. 20", 99.74999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b33.Location = new System.Drawing.Point(370, 399);
+            this.b33.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.b33.Name = "b33";
+            this.b33.Size = new System.Drawing.Size(150, 150);
+            this.b33.TabIndex = 6;
+            this.b33.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Image = global::Jogo_da_Velha.Properties.Resources.super_imgmatematica___calculo;
+            this.label1.Location = new System.Drawing.Point(136, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 35);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "x, é a sua vez";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::Jogo_da_Velha.Properties.Resources.super_imgmatematica___calculo;
+            this.ClientSize = new System.Drawing.Size(550, 609);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b31);
+            this.Controls.Add(this.b32);
+            this.Controls.Add(this.b33);
+            this.Controls.Add(this.b21);
+            this.Controls.Add(this.b22);
+            this.Controls.Add(this.b23);
+            this.Controls.Add(this.b13);
+            this.Controls.Add(this.b12);
+            this.Controls.Add(this.b11);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button b11;
+        private System.Windows.Forms.Button b12;
+        private System.Windows.Forms.Button b13;
+        private System.Windows.Forms.Button b21;
+        private System.Windows.Forms.Button b22;
+        private System.Windows.Forms.Button b23;
+        private System.Windows.Forms.Button b31;
+        private System.Windows.Forms.Button b32;
+        private System.Windows.Forms.Button b33;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
